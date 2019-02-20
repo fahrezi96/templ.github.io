06bd0abee11db05f24bb2607eede80aaa9756c19
fahrezi96 <ifmuhamad@gmail.com>
2019-02-20 09:57:40 +0700

    up responsive

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,11 +12,20 @@ $(document).ready(function() {
         main.style.minHeight = String(Number(window.innerHeight) - 114) + "px";
     });
 
-    $(".toggle-btn a").click(function() {
-        $(".sidebar-section").toggleClass("hide-nav");
-        $(".main-section").toggleClass("remove-margin");
-        // $(".nav-child-wrapper").slideUp();
-    });
+    if(Number(window.innerWidth) > 768) {
+        $(".toggle-btn a").click(function() {
+            $(".sidebar-section").toggleClass("hide-nav");
+            $(".main-section").toggleClass("remove-margin");
+            // $(".nav-child-wrapper").slideUp();
+        });
+    }
+
+    if(Number(window.innerWidth) <= 768) {
+        $(".toggle-btn a").click(function() {
+            $(".sidebar-section").toggleClass("show-nav");
+            // $(".nav-child-wrapper").slideUp();
+        });
+    }
 
     let nav = document.getElementsByClassName("sidebar-section")[0];
     nav.style.height = String(Number(window.innerHeight) - 70) + "px";
