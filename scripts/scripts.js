e34bdea94a92ee936386b149879f67047752f2c0
fahrezi96 <ifmuhamad@gmail.com>
2019-02-28 16:37:14 +0700

    up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,20 +16,20 @@ $(document).ready(function() {
     });
 
     // toggle side nav
-    // if(Number(window.innerWidth) > 768) {
+    if(Number(window.innerWidth) > 768) {
         $(".toggle-btn a").click(function() {
             sideNav.toggleClass("hide-nav");
             $(".main-container").toggleClass("remove-margin");
             // $(".collapsible-target").slideUp();
         });
-    // }
+    }
 
-    // if(Number(window.innerWidth) <= 768) {
-        // $(".toggle-btn a").click(function() {
-        //     sideNav.toggleClass("hide-nav");
+    if(Number(window.innerWidth) <= 768) {
+        $(".toggle-btn a").click(function() {
+            sideNav.toggleClass("show-nav");
             // $(".collapsible-target").slideUp();
-        // });
-    // }
+        });
+    }
 
     $(window).resize(function() { 
         // side nav container height
